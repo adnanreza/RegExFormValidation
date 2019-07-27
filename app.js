fbf0edd4c86570694bcd611cc514100246ed9e56
Adnan Reza <adnan.reza@outlook.com>
2019-07-27 09:38:08 -0700

    Implement Validate name and postal code functions

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 // Form Blur Event Listeners
 document.getElementById("name").addEventListener("blur", validateName);
-document.getElementById("zip").addEventListener("blur", validateZip);
+document
+  .getElementById("postalCode")
+  .addEventListener("blur", validatePostalCode);
 document.getElementById("email").addEventListener("blur", validateEmail);
 document.getElementById("phone").addEventListener("blur", validatePhone);
 
 function validateName() {
   const name = document.getElementById("name");
   //start with and ends with a letter and has length between 2-10
-  const re = /^[a-zA-Z]{2, 10}$/;
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add("is-invalid");
+  } else {
+    name.classList.remove("is-invalid");
+  }
 }
 
-function validateZip() {}
+function validatePostalCode() {
+  const postalCode = document.getElementById("postalCode");
+  //start with and ends with a letter and has length between 2-10
+  const re = /^[A-Z]([0-9][A-Z]){2}[0-9]$/;
+
+  if (!re.test(postalCode.value)) {
+    postalCode.classList.add("is-invalid");
+  } else {
+    postalCode.classList.remove("is-invalid");
+  }
+}
 
 function validateEmail() {}
 
